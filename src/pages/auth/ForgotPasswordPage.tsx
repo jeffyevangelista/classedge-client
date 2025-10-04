11d63a0660ddd8b602b64c0914967439b8849c46
jeffyevangelista <jevangelista@classify.com.ph>
2025-10-04 21:56:24 +0800

    fix: Added vercel.json for deployment error

--- src/pages/auth/ForgotPasswordPage.tsx
+++ src/pages/auth/ForgotPasswordPage.tsx
@@ -13,7 +13,7 @@ import { Link } from "react-router";
 const ForgotPasswordPage = () => {
   return (
     <div className="relative flex h-auto min-h-screen items-center justify-center overflow-x-hidden px-4 py-10 sm:px-6 lg:px-8">
-      <Card className="z-1 w-full border-none shadow-md sm:max-w-md">
+      <Card className="z-1 w-full border-none shadow sm:max-w-md">
         <CardHeader className="gap-6">
           <div className="flex items-center gap-2">
             <img className="size-9" src={SchoolLogo} alt="School Logo" />
